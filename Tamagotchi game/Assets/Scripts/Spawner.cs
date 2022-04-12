feabef046bad222db6167c9f81ab36b2a01f223a
josem <josema.pardo@oj2medial.com>
2022-04-12 14:09:07 -0500

    made spawner for obstacles

---
+++ Tamagotchi game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+     public GameObject[] prefab;
+     public float spawnTime;
+    public float spawnDelay;
+
+    void OnEnable()
+    {
+        //makes the script a coroutine
+        InvokeRepeating("spawn", spawnTime, spawnDelay);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawn()
+{
+
+
+    float posx = Random.Range(5, 8);
+
+    //sets variables that will be used in the new vector3
+               float positionX = posx;
+
+               //defined the vector3 position using the player's position as reference
+               Vector3 position = new Vector3(positionX, 0f, 0f);
+
+            Instantiate (prefab[Random.Range(0, 2)], position, Quaternion.identity); 
+}
+
+}
