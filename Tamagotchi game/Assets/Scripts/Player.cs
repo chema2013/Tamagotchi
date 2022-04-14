026fb35af8946ae9d011d3876e633df9eb78d5be
Anton Gschanes <antongschanes@gmail.com>
2022-04-14 01:13:48 +0100

    added death once health is 0 or lower, changed settings, fixed bugs, added functions for main menu buttons

--- Tamagotchi game/Assets/Scripts/Player.cs
+++ Tamagotchi game/Assets/Scripts/Player.cs
@@ -17,8 +17,19 @@ public class Player : MonoBehaviour
     Image StarvationBar;
     Image HealthBar;
 
+    public GameObject minigame;
+
+    public GameObject idle;
+
+    public GameObject sleep;
+
     public GameObject Asleep;
-    public GameObject Minigame;
+
+    public GameObject Eating;
+
+    public GameObject Jump;
+
+    public GameObject Death;
 
     // Start is called before the first frame update
     void Start()
@@ -44,7 +55,7 @@ public class Player : MonoBehaviour
             }
         }
 
-        if (Minigame.activeSelf)
+        if (minigame.activeSelf)
         {
             happiness += 2.25f * Time.deltaTime;
             if (happiness > 100.00f)
@@ -53,30 +64,54 @@ public class Player : MonoBehaviour
             }
         }
 
-        if(happiness < 0.00f)
+        if (happiness < 0.00f)
         {
             happiness = 0.00f;
+
+            health -= 0.80f * Time.deltaTime;
         }
+
         if (starvation < 0.00f)
         {
             starvation = 0.00f;
+
+            health -= 0.80f * Time.deltaTime;
         }
+
         if (health < 0.00f)
         {
             health = 0.00f;
+            starvation = 0.00f;
+            happiness = 0.00f;
+
+            minigame.SetActive(false);
+
+            idle.SetActive(false);
+
+            sleep.SetActive(false);
+
+            Asleep.SetActive(false);
+
+            Eating.SetActive(false);
+
+            Jump.SetActive(false);
+
+            Death.SetActive(true);
+
         }
 
-        HappinessBar.fillAmount = happiness/100.00f;
-        StarvationBar.fillAmount = starvation/100.00f;
-        HealthBar.fillAmount = health/100.00f;
+        HappinessBar.fillAmount = happiness / 100.00f;
+        StarvationBar.fillAmount = starvation / 100.00f;
+        HealthBar.fillAmount = health / 100.00f;
     }
 
     public void Feed()
     {
         starvation += 30.00f;
-        if(starvation > 100.00f)
+        if (starvation > 100.00f)
         {
             starvation = 100.00f;
         }
     }
+
 }
